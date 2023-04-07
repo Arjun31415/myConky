99c26647baad3b66ab4641576ec64be47ac6b785
Arjun31415 <arjunp0710@gmail.com>
2023-04-07 10:55:40 +0530

    few updates

--- scripts/get_assignments.py
+++ scripts/get_assignments.py
@@ -1,12 +1,11 @@
 #!/home/arjun/myConky/.venv/bin/python3
 import sys
 import os
-import pprint
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 
 load_dotenv()
